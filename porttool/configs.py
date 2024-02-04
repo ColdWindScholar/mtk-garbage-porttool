00fcc097a5f18af67c00586d3ae58f55b884b851
ColdWindScholar <3590361911@qq.com>
2024-02-04 12:07:43 +0800

    [tool] Update Some Code

--- porttool/configs.py
+++ porttool/configs.py
@@ -1,8 +1,31 @@
+import platform
 from os import getcwd
 import os.path as op
-from . import archdetect
-
 import json
+
+
+def retTypeAndMachine():
+    # Detect machine and ostype
+    ostype = platform.system().lower()
+    if ostype.find("cygwin") >= 0:  # Support cygwin x11
+        ostype = "windows"
+    machine = platform.machine().lower()
+    if machine in ['aarch64_be', 'armv8b', 'armv8l']:
+        machine = 'aarch64'
+    if machine in ['i386', 'i686']:
+        machine = 'x86'
+    if machine == "amd64":
+        machine = 'x86_64'
+    if ostype == 'windows':
+        if not machine == 'x86_64':
+            print("Error : Program on windows only support 64bit machine\n")
+            return None
+    if ostype == 'linux':
+        if machine not in ['aarch64', 'arm', 'x86', 'x86_64']:
+            print("Error : Machine not support your device [%s]\n" % machine)
+            return None
+    return ostype.replace('windows', 'win'), machine
+
 
 # configs for porttool
 support_chipset_portstep = {
@@ -219,7 +242,7 @@ else:
 
 support_chipset = list(support_chipset_portstep.keys())
 support_packtype = ['zip', 'img']
-ostype, arch = archdetect.retTypeAndMachine()
+ostype, arch = retTypeAndMachine()
 ext_ext = '.exe' if ostype == 'win' else ''
 
 # binarys
